136b6af4d9ec9197868442d6faa258d251994eb3
Aleksey Shmykov <shmykov.job@gmail.com>
2021-11-20 15:32:33 +0300

    fix errors before: -make models and migrations -worked with models in console -create admin tool and some product on it

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
-MEDIA_URL = '/media/'
+MEDIA_URL ='/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
