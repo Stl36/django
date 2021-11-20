1dc1d1ca07eec3de96ea1a9bae3e6089a0617402
Aleksey Shmykov <shmykov.job@gmail.com>
2021-11-20 17:05:28 +0300

    connect DB in views.py and update products.html template

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,6 +1,6 @@
 from django.core.management.commands import loaddata
 from django.shortcuts import render
-
+from .models import ProductCategory, Product
 # Create your views here.
 
 
@@ -12,33 +12,9 @@ def index(request):
 
 
 def products(request):
+    products = Product.objects.all()
     context = {
         'title': 'GeekShop - Каталог',
-        'products':[
-            {'name':'Худи черного цвета с монограммами adidas Originals',
-             'price': 6090,
-             'mini_desc': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-             'img_url' : '/vendor/img/products/Adidas-hoodie.png'},
-
-            {'name': 'Синяя куртка The North Face',
-             'price': 23725,
-             'mini_desc': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-             'img_url': '/vendor/img/products/Blue-jacket-The-North-Face.png'},
-
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-             'price': 3390,
-             'mini_desc': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-             'img_url': '/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'},
-
-            {'name': 'Черный рюкзак Nike Heritage',
-             'price': 2340,
-             'mini_desc': 'Плотная ткань. Легкий материал.',
-             'img_url': '/vendor/img/products/Black-Nike-Heritage-backpack.png'},
-
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-             'price': 13590,
-             'mini_desc': 'Гладкий кожаный верх. Натуральный материал.',
-             'img_url': '/vendor/img/products/Black-Dr-Martens-shoes.png'},
-        ]
+        'products': products
     }
     return render(request, 'mainapp/products.html', context)
