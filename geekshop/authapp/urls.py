7a32ef3ec64c0fd978957688bed5643f87c432a8
Aleksey Shmykov <shmykov.job@gmail.com>
2021-11-21 20:32:23 +0300

    auth urls.py setup

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
 # from authapp.views import
-
+from authapp.views import login, register
 
 app_name = 'authapp'
 urlpatterns = [
 
-    # path('', products, name='authapp'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
