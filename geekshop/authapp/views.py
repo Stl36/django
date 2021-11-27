ff77269f72600c33f0b93d538b8fc26f97a0a47e
Aleksey Shmykov <shmykov.job@gmail.com>
2021-11-27 16:41:19 +0300

    make success alert

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,4 +1,4 @@
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -44,6 +44,7 @@ def register(request):
         form = UserRegisterForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request,'Вы успешно зарегестрировались')
             return HttpResponseRedirect(reverse('authapp:login'))
         # else:
         #     # print(form.errors)
