d17b25fa22400cb7b5560d042093ad815129cfe8
Aleksey Shmykov <shmykov.job@gmail.com>
2021-12-04 17:03:31 +0300

    modify add product to basket to button and fix aito reload basket

--- geekshop/baskets/views.py
+++ geekshop/baskets/views.py
@@ -10,27 +10,47 @@ from baskets.models import Basket
 from mainapp.models import Product
 
 
+# @login_required
+# def basket_add(request, id):
+#     user_select = request.user
+#     product = Product.objects.get(id=id)
+#     baskets = Basket.objects.filter(user=user_select, product=product)
+#
+#     if baskets:
+#         basket = baskets.first()
+#         basket.quantity += 1
+#         basket.save()
+#     else:
+#         Basket.objects.create(user=user_select, product=product, quantity=1)
+#
+#     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 @login_required
 def basket_add(request, id):
-    user_select = request.user
-    product = Product.objects.get(id=id)
-    baskets = Basket.objects.filter(user=user_select, product=product)
+    if request.is_ajax():
 
-    if baskets:
-        basket = baskets.first()
-        basket.quantity += 1
-        basket.save()
-    else:
-        Basket.objects.create(user=user_select, product=product, quantity=1)
+        user_select = request.user
+        product = Product.objects.get(id=id)
+        baskets = Basket.objects.filter(user=user_select, product=product)
 
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        if baskets:
+            basket = baskets.first()
+            basket.quantity += 1
+            basket.save()
+        else:
+            Basket.objects.create(user=user_select, product=product, quantity=1)
+
+        products = Product.objects.all()
+        context = {'products': products}
+        result = render_to_string('mainapp/includes/card.html', context)
+        return JsonResponse({'result': result})
 
 
 @login_required
-def basket_remove(request,basket_id):
-
+def basket_remove(request, basket_id):
     Basket.objects.get(id=basket_id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 @login_required
 def basket_edit(request, id_basket, quantity):
@@ -43,9 +63,6 @@ def basket_edit(request, id_basket, quantity):
             basket.delete()
 
         baskets = Basket.objects.filter(user=request.user)
-        context = {
-            'baskets':baskets
-        }
-        result = render_to_string('baskets/basket.html',context)
-        return JsonResponse({'result':result})
-
+        context = {'baskets': baskets}
+        result = render_to_string('baskets/basket.html', context)
+        return JsonResponse({'result': result})
--- geekshop/static/js/basket.js
+++ geekshop/static/js/basket.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    $('.basket-list').on('click', 'input[type="number"]', function () {
+    $('.basket_list').on('click', 'input[type="number"]', function () {
 
         let t_href = event.target
 
@@ -12,6 +12,23 @@ window.onload = function () {
                 url: "/baskets/edit/" + t_href.name + "/" + t_href.value + "/",
                 success: function (data) {
                     $('.basket_list').html(data.result)
+                },
+
+            });
+        event.preventDefault()
+
+    })
+
+        $('.card_add_basket').on('click', 'button[type="button"]', function () {
+
+        let t_href = event.target.value
+        alert('Товар добавлен в корзину')
+
+        $.ajax(
+            {
+                url: "/baskets/add/" + t_href + "/",
+                success: function (data) {
+                    $('.card_add_basket').html(data.result)
                 },
 
             });
