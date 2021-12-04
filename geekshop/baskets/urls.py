c50283127f7956a8141c3c47fc48c0baa06d1856
Aleksey Shmykov <76962941+Stl36@users.noreply.github.com>
2021-12-04 17:57:36 +0300

    Lesson 6 2 1 (#9)
    * ask for someone
    
    * ask for someone
    
    * modify add product to basket to button and fix aito reload basket
    
    * took out the footer
    
    * add detail.html
    
    * setup for detail.html

--- geekshop/baskets/urls.py
+++ geekshop/baskets/urls.py
@@ -9,7 +9,7 @@ app_name = 'baskets'
 urlpatterns = [
 
 
-    path('add/<int:id>', basket_add, name='basket_add'),
+    path('add/<int:id>/', basket_add, name='basket_add'),
     path('remove/<int:basket_id>', basket_remove, name='basket_remove'),
     path('edit/<int:id_basket>/<int:quantity>/', basket_edit, name='basket_edit'),
 
--- geekshop/static/js/basket.js
+++ geekshop/static/js/basket.js
@@ -1,12 +1,12 @@
 window.onload = function () {
-    $('.basket-list').on('click', 'input[type="number"]', function () {
+    $('.basket_list').on('click', 'input[type="number"]', function () {
 
         let t_href = event.target
 
         console.log(t_href.name)
         console.log(t_href.value)
 
-
+// Код ниже не отрисовывает корзину при обновлении автоматически. Почему?
         $.ajax(
             {
                 url: "/baskets/edit/" + t_href.name + "/" + t_href.value + "/",
@@ -18,4 +18,22 @@ window.onload = function () {
         event.preventDefault()
 
     })
+
+        $('.card_add_basket').on('click', 'button[type="button"]', function () {
+
+        let t_href = event.target.value
+        alert('Товар добавлен в корзину')
+
+        $.ajax(
+            {
+                url: "/baskets/add/" + t_href + "/",
+                success: function (data) {
+                    $('.card_add_basket').html(data.result)
+                },
+
+            });
+        event.preventDefault()
+
+    })
+
 }
