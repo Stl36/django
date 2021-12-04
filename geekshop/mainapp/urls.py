ef803cf5fcb5728b8b27d00486da4c66498ac445
Aleksey Shmykov <shmykov.job@gmail.com>
2021-12-04 17:54:22 +0300

    setup for detail.html

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from mainapp.views import index, products
+from mainapp.views import index, products, ProductDetail
 
 app_name = 'mainapp'
 urlpatterns = [
     path('', products, name='products'),
+    path('detail/<int:pk>', ProductDetail.as_view(), name='detail'),
+
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,6 +1,10 @@
 from django.core.management.commands import loaddata
 from django.shortcuts import render
 from .models import ProductCategory, Product
+import os
+from django.views.generic import DetailView
+
+MODULE_DIR = os.path.dirname(__file__)
 
 
 # Create your views here.
@@ -21,3 +25,17 @@ def products(request):
         'categories': ProductCategory.objects.all(),
     }
     return render(request, 'mainapp/products.html', context)
+
+
+class ProductDetail(DetailView):
+    """
+    Контроллер вывода инфо о продукте
+    """
+    model = Product
+    template_name = 'mainapp/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        product = self.get_object()
+        context['product'] = product
+        return context
