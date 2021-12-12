f56181f38df31a997e16792da8251e5142edb4aa
Aleksey Shmykov <shmykov.job@gmail.com>
2021-12-12 17:54:31 +0300

    paginator

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from mainapp.views import index, products, ProductDetail
+from mainapp.views import products, ProductDetail
 
 app_name = 'mainapp'
 urlpatterns = [
+
     path('', products, name='products'),
     path('category/<int:id_category>', products, name='category'),
-    path('detail/<int:pk>', ProductDetail.as_view(), name='detail'),
-
+    path('page/<int:page>', products, name='page'),
+    path('detail/<int:pk>/', ProductDetail.as_view(), name='detail'),
 ]
