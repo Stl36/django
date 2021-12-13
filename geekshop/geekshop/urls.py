0f298e2cf39bb50879738deb04f946354405db1c
Aleksey Shmykov <shmykov.job@gmail.com>
2021-12-13 13:17:13 +0300

    up

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.i18n import set_language
 
 from mainapp.views import index, products
 
@@ -27,6 +29,11 @@ urlpatterns = [
     path('users/', include('authapp.urls', namespace='authapp')),
     path('baskets/', include('baskets.urls', namespace='baskets')),
     path('admins/', include('admins.urls', namespace='admins')),
+
+    # path('language_new/', include('django.conf.urls',namespace='language_new')),
+    # path('lang/', set_language, name='language'),
+    # path(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 
 ]
 
