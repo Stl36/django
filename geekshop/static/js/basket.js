275aed786d3eabb89c0690d412775d817240f38e
Aleksey Shmykov <shmykov.job@gmail.com>
2021-12-13 16:51:57 +0300

    up

--- geekshop/static/js/basket.js
+++ geekshop/static/js/basket.js
@@ -1,41 +1,44 @@
 window.onload = function () {
     $('.basket_list').on('click', 'input[type="number"]', function () {
-
         let t_href = event.target
-
-        console.log(t_href.name)
-        console.log(t_href.value)
-
-// Код ниже не отрисовывает корзину при обновлении автоматически. Почему?
-//        Ответ найден. Была ошибка в названии класса
-
         $.ajax(
             {
                 url: "/baskets/edit/" + t_href.name + "/" + t_href.value + "/",
                 success: function (data) {
                     $('.basket_list').html(data.result)
                 },
-
             });
         event.preventDefault()
-
     })
 
-        $('.card_add_basket').on('click', 'button[type="button"]', function () {
-
+    $('.card_add_basket').on('click', 'button[type="button"]', function () {
         let t_href = event.target.value
-        alert('Товар добавлен в корзину')
-
         $.ajax(
             {
                 url: "/baskets/add/" + t_href + "/",
                 success: function (data) {
                     $('.card_add_basket').html(data.result)
+                    alert('товар добавлен вы корзину')
                 },
-
             });
         event.preventDefault()
-
+    //
     })
+    // var csrf = $('meta[name="csrf-token"]').attr('content');
+    // $('.card_add_basket').on('click', 'button[type="button"]', function () {
+    //     let t_href = event.target.value
+    //     $.ajax(
+    //         {   type: 'POST',
+    //             headers: {"X-CSRFToken": csrf},
+    //             url: "/baskets/add/" + t_href + "/",
+    //             success: function (data) {
+    //                 $('.card_add_basket').html(data.result)
+    //                 alert('товар добавлен вы корзину')
+    //             },
+    //         });
+    //     event.preventDefault()
+    //
+    // })
+
 
 }
