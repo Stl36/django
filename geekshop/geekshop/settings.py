d928bb4e91b1b60e41875c144fb5ba4c4a0d4b5b
Aleksey Shmykov <shmykov.job@gmail.com>
2021-12-15 20:40:29 +0300

    1 commit

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -145,16 +145,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authapp.User'
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
-
-DOMAIN_NAME = 'http://localhost:8000'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 25
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
-
-# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-# EMAIL_FILE_PATH = 'tmp/emails/'
-
-EMAIL_HOST_USER ,EMAIL_HOST_PASSWORD = None,None
-# python -m smtpd -n -c DebuggingServer localhost:25
+#
+# DOMAIN_NAME = 'http://localhost:8000'
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT = 25
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
+#
+# # EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# # EMAIL_FILE_PATH = 'tmp/emails/'
+#
+# EMAIL_HOST_USER ,EMAIL_HOST_PASSWORD = None,None
+# # python -m smtpd -n -c DebuggingServer localhost:25
+#
+VKSOCIAL_AUTH_VK_OAUTH2_KEY = ''
